e03c6e66df9dd572b510d7017fa79bed197064a9
Nikola104 <asd@abv.bg>
2023-12-14 20:34:06 +0200

    permissions and comments

--- src/main/java/eCommerce/com/eCommerce/auth/AuthenticationController.java
+++ src/main/java/eCommerce/com/eCommerce/auth/AuthenticationController.java
@@ -3,11 +3,15 @@ package eCommerce.com.eCommerce.auth;
 import eCommerce.com.eCommerce.dto.request.AuthenticationRequest;
 import eCommerce.com.eCommerce.dto.request.RegistrationRequest;
 import eCommerce.com.eCommerce.dto.response.AuthenticationResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping(path = "api/v1/auth")
@@ -29,6 +33,11 @@ public class AuthenticationController {
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
 
         return ResponseEntity.ok(authenticationService.authenticate(request));
+
+    }
+    @PostMapping("/refresh-token")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        authenticationService.refreshToken(request, response);
 
     }
 
--- src/main/java/eCommerce/com/eCommerce/service/UserService.java
+++ src/main/java/eCommerce/com/eCommerce/service/UserService.java
@@ -3,9 +3,13 @@ package eCommerce.com.eCommerce.service;
 import eCommerce.com.eCommerce.model.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 public interface UserService {
 
     public void save(User user);
     public User findByEmail(String email);
+
+    List<User> getAllUsers();
 }
