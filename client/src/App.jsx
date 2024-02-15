d098dbf79d8e67763d00c0fdedf04b5a646a7afa
nikola104 <vulchinovn@gmail.com>
2024-02-15 22:49:42 +0200

    Adding register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 
-  return (
+import AuthGuardWhenLogout from './components/authguard/AuthGuardWhenLogout';
+import AuthGuardWhenLogin from './components/authguard/AuthGuardWhenLogin';
+import CustomerRegister from './pages/register/CustomerRegister';
+import CustomerLogin from './pages/Login/CustomerLogin';
+
+
+
+const App = createBrowserRouter(
+  createRoutesFromElements(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Route element={<AuthGuardWhenLogout />}>
+          <Route path="/">
+            <Route path="login" element={<CustomerLogin/>} />
+            <Route path="register" element={<CustomerRegister/>} />
+          
+          </Route>
+
+        </Route>
+          <Route element={<AuthGuardWhenLogin />}>
+        
+        </Route>
     </>
   )
-}
-
+);
 export default App
